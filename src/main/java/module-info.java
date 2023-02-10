85d8bdfa096084cdb1f6c4dbda77b24be9aa068e
Valentina Cadena <v.cadenafajardo@student.maastrichtuniversity.nl>
2023-02-10 11:18:47 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.dacs.digitalassistent {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.dacs.digitalassistent to javafx.fxml;
+    exports com.dacs.digitalassistent;
+}
