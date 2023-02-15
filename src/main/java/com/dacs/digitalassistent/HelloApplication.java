dec2e2d8e53077da9b8812e4073117e8b4eeb000
Valentina Cadena <v.cadenafajardo@student.maastrichtuniversity.nl>
2023-02-15 11:24:35 +0100

    Added outputMessage function

--- src/main/java/com/dacs/digitalassistent/HelloApplication.java
+++ src/main/java/com/dacs/digitalassistent/HelloApplication.java
@@ -23,6 +23,10 @@ import java.awt.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private Image icon = new Image("C:\\Users\\vales\\Documents\\Java Projects\\DigitalAssistent\\src\\main\\java\\com\\dacs\\digitalassistent\\blue-eye-original-icon.png");
+    private VBox conversation = new VBox();
+
     @Override
     public void start(Stage stage) throws IOException {
         HBox root = new HBox();
@@ -39,32 +43,26 @@ public class HelloApplication extends Application {
         // Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("DAC°Search");
 
-        Image icon = new Image("C:\\Users\\vales\\Documents\\Java Projects\\DigitalAssistent\\src\\main\\java\\com\\dacs\\digitalassistent\\blue-eye-original-icon.png");
         ImageView iconView = new ImageView(icon);
         iconView.setFitWidth(icon.getWidth() * 0.2);
         iconView.setFitHeight(icon.getHeight() * 0.2);
-        ImageView iconTextView = new ImageView(icon);
-        iconTextView.setFitWidth(icon.getWidth() * 0.05);
-        iconTextView.setFitHeight(icon.getHeight() * 0.05);
+
         Image restartIcon = new Image("C:\\Users\\vales\\Documents\\Java Projects\\DigitalAssistent\\src\\main\\java\\com\\dacs\\digitalassistent\\restart-icon.png");
         ImageView restartIconView = new ImageView(restartIcon);
         restartIconView.setFitWidth(restartIcon.getWidth() * 0.1);
         restartIconView.setFitHeight(restartIcon.getHeight() * 0.1);
+
         Image backgroundImage = new Image("C:\\Users\\vales\\Documents\\Java Projects\\DigitalAssistent\\src\\main\\java\\com\\dacs\\digitalassistent\\fog-dark.png");
         BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(1, 1, true, true, false, false));
-        Text textInit = new Text();
-        textInit.setText("Hello, how can I help you?");
-        textInit.setFill(Color.WHITE);
-        textInit.setFont(Font.font("Courier New", 15));
-        HBox initIconText = new HBox(iconTextView, textInit);
-        VBox conversation = new VBox(initIconText);
+
         VBox iconTop = new VBox(iconView);
         iconTop.setAlignment(Pos.CENTER);
         VBox iconTopTextBottom = new VBox(iconTop, conversation);
         iconTopTextBottom.setMargin(conversation, new Insets(15, 10, 10, 10));
         iconTopTextBottom.setMargin(iconTop, new Insets(5, 0, 0, 0));
         paneLeft.setMargin(restartIconView, new Insets(15, 0, 0, 0));
-        initIconText.setMargin(textInit, new Insets(0, 0, 0, 5));
+
+        outputMessage("Hello, how can I help you?");
 
         stage.getIcons().add(icon);
         paneLeft.setMinWidth(scene.getWidth() * 0.1);
@@ -77,6 +75,20 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
 
         stage.show();
+    }
+
+    public void outputMessage(String message) {
+        Text text = new Text();
+        text.setText(message);
+        text.setFill(Color.WHITE);
+        text.setFont(Font.font("Courier New", 15));
+        ImageView iconView = new ImageView(icon);
+        iconView.setFitWidth(icon.getWidth() * 0.05);
+        iconView.setFitHeight(icon.getHeight() * 0.05);
+        HBox iconText = new HBox(iconView, text);
+        iconText.setMargin(text, new Insets(0, 0, 0, 5));
+        conversation.getChildren().add(iconText);
+        conversation.setMargin(iconText, new Insets(5, 0, 5, 0));
     }
 
     public static void main(String[] args) {
