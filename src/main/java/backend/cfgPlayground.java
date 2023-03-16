b364abc35539359d0c95e79e61d7fca3ba5200a6
Simon-Garland <91745165+Simon-Garland@users.noreply.github.com>
2023-03-16 13:28:57 +0100

    Add files via upload

--- src/main/java/backend/cfgPlayground.java
+++ src/main/java/backend/cfgPlayground.java
@@ -26,13 +26,66 @@ package backend;
 
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import UI.HelloApplication;
+
 // THIS CLASS IS JUST A SCRATCH. IGNORE IT
 public class cfgPlayground {
-    public static void main(String[] args) {
-        String input = "play classical music";
-        // regex syntax
-        if(input.matches("play (rock|pop|jazz|classical) music")){
-            System.out.println("Playing");
+    public static void main(String[] args) throws IOException {
+        int max = 0;
+        int count = 0;
+        ArrayList<String> keywords = new ArrayList<>();
+        ArrayList<String> userkeys = new ArrayList<>();
+        ArrayList<String[]> outputkeys = new ArrayList<>();
+        ArrayList<String> actions = new ArrayList<>();
+        //String user = HelloApplication.getUserMessage();
+        String question = "";
+        String answer = "";
+        Scanner reader = new Scanner(new File("C:\\Users\\simon\\Downloads\\PROJECT-2-2_Group_10-main-2\\src\\main\\java\\backend\\Skills\\SkillsTemplate.txt"));
+            while(reader.hasNextLine()) {
+               String word = reader.nextLine();
+             if(word.contains("Slot ")) {
+                keywords.add(word.replaceAll("Slot |<.*?> ",""));
+               }
+             if(word.contains("Question ")) {
+                 question = word.replace("Question ", "");
+             }
+             if(word.contains("Action ")) {
+                 actions.add(word.replaceAll("Action |<.*?> ",""));
+             }
+            }
+
+        for(int i = 0; i < keywords.size(); i++) {
+            if(question.contains(keywords.get(i))) {
+                userkeys.add(keywords.get(i));
+            }
         }
+
+        for(int i = 0; i < actions.size(); i++) {
+          for(int j = 0; j < userkeys.size(); j++) {
+              if(actions.get(i).contains(userkeys.get(j))) {
+                  count++;
+              }
+              if(count > max) {
+                  max = count;
+                  answer = actions.get(i);
+
+              }
+              count = 0;
+          }
+        }
+        if(max == 0) {
+            System.out.println(actions.get(actions.size()-1));
+        }
+        else {
+            System.out.println(answer);
+        }
+        reader.close();
     }
 }
