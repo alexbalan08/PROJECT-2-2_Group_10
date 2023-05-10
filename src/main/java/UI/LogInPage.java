52390902986191dde9b9ca2f5412c9e98c500545
Valentina Cadena <v.cadenafajardo@student.maastrichtuniversity.nl>
2023-05-10 19:48:18 +0200

    Connected login face detection with HelloApplication

--- src/main/java/UI/LogInPage.java
+++ src/main/java/UI/LogInPage.java
@@ -5,15 +5,10 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-//import javafx.scene.image.Image;
-import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -22,7 +17,6 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
@@ -35,12 +29,13 @@ import java.io.IOException;
 
 public class LogInPage extends Application {
     private String font = "Courier New";
-    private HelloApplication main;
     private Stage primaryStage;
+    private HelloApplication main;
     private ImageView imageView;
     private VideoCapture capture;
     private ScheduledExecutorService executor;
-
+    private boolean windowOpened = false;
+    private boolean faceRecognition = true;
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -54,70 +49,11 @@ public class LogInPage extends Application {
 
         this.primaryStage = primaryStage;
 
-//        GridPane grid = new GridPane();
-//        grid.setAlignment(Pos.CENTER);
-//
-//        grid.setHgap(20);
-//        grid.setVgap(20);
-//        grid.setPadding(new Insets(25, 25, 25, 25));
-//
-//
-//        Label userName = new Label("User Name:");
-//        userName.setFont(Font.font(font, 20));
-//        userName.setTextFill(Color.WHITE);
-//
-//
-//        grid.add(userName, 0, 1);
-//        TextField userTextField = new TextField();
-//        userTextField.setStyle("-fx-background-color: rgba(55, 180, 220, 0.15); -fx-text-fill: white");
-//        grid.add(userTextField, 1, 1);
-//        Label pw = new Label("Password:");
-//        pw.setFont(Font.font(font, 20));
-//        pw.setTextFill(Color.WHITE);
-//        grid.add(pw, 0, 2);
-//        PasswordField pwBox = new PasswordField();
-//        pwBox.setStyle("-fx-background-color: rgba(55, 180, 220, 0.15); -fx-text-fill: white");
-//        grid.add(pwBox, 1, 2);
-//
-//        // Add the login button
-//        Button btn = new Button("Login");
-//        btn.setFont(Font.font(font));
-//        btn.setStyle("-fx-background-color: rgba(55, 180, 220, 0.15); -fx-text-fill: white");
-//        HBox hbBtn = new HBox(20);
-//        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-//        hbBtn.getChildren().add(btn);
-//        grid.add(hbBtn, 1, 4);
-//
-//        // Add a label for displaying login status
-//        final Label message = new Label();
-//        grid.add(message, 1, 6);
-//
-//
-//        btn.setOnAction(event -> {
-//            String userNameValue = userTextField.getText();
-//            String passwordValue = pwBox.getText();
-//            if (true/*userNameValue.equals("team10") && passwordValue.equals("dacs")*/) {
-//                message.setText("Login successful!");
-//                primaryStage.hide();
-//                try {
-//                    main.start(primaryStage);
-//                } catch (IOException | NoSuchMethodException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//            } else {
-//                message.setText("Incorrect username or password.");
-//            }
-//            message.setFont(Font.font(font));
-//            message.setTextFill(Color.WHITE);
-//        });
-
-
         Image eyeimage= new Image("file:src\\main\\resources\\login\\login\\img.png");
 
         ImageView bum=new ImageView(eyeimage);
-        bum.setFitWidth(eyeimage.getWidth() * 0.4);
-        bum.setFitHeight(eyeimage.getHeight() * 0.4);
+        bum.setFitWidth(eyeimage.getWidth() * 0.27);
+        bum.setFitHeight(eyeimage.getHeight() * 0.27);
         bum.setSmooth(true);
 
         StackPane eyePane = new StackPane(bum);
@@ -129,9 +65,20 @@ public class LogInPage extends Application {
 
         imageView = new ImageView();
 
+        StackPane cameraPane = new StackPane(imageView);
+        eyePane.setAlignment(Pos.CENTER);
+
+        Label message = new Label();
+        message.setText("Approach your face to log in.");
+        message.setFont(Font.font(font, 16));
+        message.setTextFill(Color.WHITE);
+
+        VBox content = new VBox(20);
+        content.getChildren().addAll(eyePane, cameraPane, message);
         StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(backgroundPane, eyePane, imageView);
-        stackPane.setMargin(eyePane, new Insets(bum.getFitHeight() * 0.6, 0, bum.getFitHeight() * 0.4, 0));
+        stackPane.getChildren().addAll(backgroundPane, content);
+        content.setMargin(eyePane, new Insets(bum.getFitHeight() * 0.3, 0, 0, 0));
+        content.setMargin(message, new Insets(0, 0, 0, bum.getFitHeight() * 0.7));
 
         Scene scene = new Scene(stackPane, 600, 600);
 
@@ -169,67 +116,61 @@ public class LogInPage extends Application {
             MatOfRect faceDetection = new MatOfRect();
             faceDetector.detectMultiScale(grayFrame, faceDetection, 1.1, 3, 0, new Size(30, 30), new Size());
 
-            System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
+//            System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
 
             for (Rect rect : faceDetection.toArray()) {
                 Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
+                if (!windowOpened) {
+                    windowOpened = true;
+
+                    // Open a new JavaFX window when a face is detected
+                    Platform.runLater(() -> {
+                        try {
+                            openNewWindow();
+                        } catch (IOException | NoSuchMethodException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
             }
 
-//            if (faceDetection.toArray().length > 0) {
-////                final Label message = new Label();
-////                message.setText("Login successful!");
-//                try {
-//                    executor.shutdown();
-//                    executor.awaitTermination(500, TimeUnit.MILLISECONDS);
-//
-//                    if (capture.isOpened()) {
-//                        capture.release();
-//                    }
-//                } catch (InterruptedException | IllegalStateException e) {
-//                    e.printStackTrace();
-//                }
-////                primaryStage.hide();
-////                try {
-////                    main.start(primaryStage);
-////                } catch (IOException | NoSuchMethodException e) {
-////                    throw new RuntimeException(e);
-////                }
-//
-//            }
-
-//            Imgcodecs.imwrite("src/main/resources/faceImages/womanFaceOutput.png", frame);
-//            System.out.println("Done");
-
             // Convert the frame to JavaFX Image for display
-            Image image = matToJavaFXImage(frame);
+            Image image = matToJavaFXImage(frame, 0.8);
 
             Platform.runLater(() -> imageView.setImage(image));
         }
     }
 
-    private Image matToJavaFXImage(Mat frame) {
+    private Image matToJavaFXImage(Mat frame, double scalar) {
         int width = frame.width();
         int height = frame.height();
         int channels = frame.channels();
         byte[] sourcePixels = new byte[width * height * channels];
         frame.get(0, 0, sourcePixels);
 
-        WritableImage image = new WritableImage(width, height);
+        int targetWidth = (int) (width * scalar);
+        int targetHeight = (int) (height * scalar);
+
+        WritableImage image = new WritableImage(targetWidth, targetHeight);
         PixelWriter pixelWriter = image.getPixelWriter();
 
         if (channels == 1) {
             // Grayscale image
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int grayValue = sourcePixels[y * width + x] & 0xFF;
+            for (int y = 0; y < targetHeight; y++) {
+                for (int x = 0; x < targetWidth; x++) {
+                    int sourceX = (int) (x * ((double) width / targetWidth));
+                    int sourceY = (int) (y * ((double) height / targetHeight));
+                    int grayValue = sourcePixels[sourceY * width + sourceX] & 0xFF;
                     pixelWriter.setColor(x, y, javafx.scene.paint.Color.rgb(grayValue, grayValue, grayValue));
                 }
             }
         } else if (channels == 3) {
             // BGR image
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int index = (y * width + x) * channels;
+            for (int y = 0; y < targetHeight; y++) {
+                for (int x = 0; x < targetWidth; x++) {
+                    int sourceX = (int) (x * ((double) width / targetWidth));
+                    int sourceY = (int) (y * ((double) height / targetHeight));
+                    int index = (sourceY * width + sourceX) * channels;
                     int blue = sourcePixels[index] & 0xFF;
                     int green = sourcePixels[index + 1] & 0xFF;
                     int red = sourcePixels[index + 2] & 0xFF;
@@ -239,5 +180,14 @@ public class LogInPage extends Application {
         }
 
         return image;
+    }
+
+    private void openNewWindow() throws IOException, NoSuchMethodException {
+        // Create a new instance of HelloApplication and show the window
+        Stage stage = new Stage();
+        main.start(stage);
+
+        // Close the initial window
+        primaryStage.close();
     }
 }
