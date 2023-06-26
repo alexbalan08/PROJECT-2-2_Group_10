521ca2db0c5af9f5ef4cc0193ba3a0956625775b
LiutaurasP <91745143+LiutaurasP@users.noreply.github.com>
2023-06-26 19:59:04 +0200

    Made ASR class more readable and fixed minor bugs.

--- src/main/java/UI/WhisperAI.java
+++ src/main/java/UI/WhisperAI.java
@@ -4,91 +4,67 @@ import javax.sound.sampled.*;
 import java.io.*;
 
 public class WhisperAI {
+    public void record() {
+        byte[] b = new byte[1024];
+        ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
+        try {
+            AudioFormat audioSettings = new AudioFormat(16000, 16, 1, true, true);
+            TargetDataLine audio = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, audioSettings));
 
-        public static void main(String[] args) throws IOException {
-            WhisperAI test = new WhisperAI();
-            test.record();
-            test.startASR();
-        }
+            audio.open(audioSettings);
+            audio.start();
 
-        public void record (){
-            int SAMPLE_RATE = 16000;
-            try {
-                // Open the audio input line
-
-                AudioFormat format = new AudioFormat(SAMPLE_RATE, 16, 1, true, true);
-                DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-                TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
-                line.open(format);
-
-                // Start capturing the audio
-                line.start();
-
-                System.out.println("Recording started. Press Enter to stop.");
-
-                // Create a byte array to store the captured audio
-                byte[] buffer = new byte[1024];
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-                // Read the captured audio and write it to the output stream
-                while (HelloApplication.getInstance().counter == 1) {
-                    int bytesRead = line.read(buffer, 0, buffer.length);
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-
-                // Stop capturing the audio
-                line.stop();
-                line.close();
-
-                System.out.println("Recording stopped.");
-
-                // Save the captured audio to a file
-                byte[] audioData = outputStream.toByteArray();
-                AudioInputStream audioStream = new AudioInputStream(
-                        new ByteArrayInputStream(audioData), format, audioData.length);
-                AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, new File("C:\\Users\\simon\\OneDrive\\Documents\\Sound Recordings\\Recording.m4a"));
-
-                System.out.println("Recording saved to output.wav");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        public String startASR() throws IOException {
-            try {
-                // Specify the directory for the cmd process
-                File directory = new File("C:/Users/simon/OneDrive/Documents/Sound Recordings");
-
-                // Create the ProcessBuilder with the command you want to execute
-                ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "whisper", "Recording.m4a", "--language", "English", "--model", "base.en","--output_format", "txt");
-
-                // Set the directory for the process
-                processBuilder.directory(directory);
-
-                // Redirect the output of the command to the Java process
-                processBuilder.redirectErrorStream(true);
-
-                // Start the process
-                Process process = processBuilder.start();
-
-                // Read the output of the process
-                InputStream inputStream = process.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                process.waitFor();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
+            while (HelloApplication.getInstance().counter == 1) {
+                int bytesRead = audio.read(b, 0, b.length);
+                outputArray.write(b, 0, bytesRead);
             }
 
-            BufferedReader br = new BufferedReader(new FileReader("C:/Users/simon/OneDrive/Documents/Sound Recordings/Recording.txt"));
-            System.out.println("Extracted text: ");
-            String line;
-            while ((line = br.readLine()) != null) {
-                return line;
-            }
-            return "Sorry, couldn't understand you...";
+            audio.stop();
+            audio.close();
+
+            byte[] data = outputArray.toByteArray();
+            AudioInputStream finalStream = new AudioInputStream(new ByteArrayInputStream(data), audioSettings, data.length);
+            AudioSystem.write(finalStream, AudioFileFormat.Type.WAVE, new File("C:\\Users\\simon\\OneDrive\\Documents\\Sound Recordings\\Recording.m4a"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("End of the recording.");
         }
     }
+
+    public String startASR() throws IOException {
+        try {
+            // Preparing a call to cmd
+            ProcessBuilder cmd = new ProcessBuilder("cmd", "/c", "whisper", "Recording.m4a", "--language", "English", "--model", "base.en", "--output_format", "txt");
+            cmd.directory(new File("C:/Users/simon/OneDrive/Documents/Sound Recordings"));
+            cmd.redirectErrorStream(true);
+            Process process = cmd.start();
+
+            // Extracted text from command line.
+            BufferedReader brCMD = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String textFromCMD;
+            while ((textFromCMD = brCMD.readLine()) != null) {
+                System.out.println(textFromCMD);
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Reading out the transcript of the recording.
+        BufferedReader brTXT = new BufferedReader(new FileReader("C:/Users/simon/OneDrive/Documents/Sound Recordings/Recording.txt"));
+        System.out.println("Extracted text: ");
+        String textFromTXT = "";
+        String reading;
+        while ((reading = brTXT.readLine()) != null) {
+            textFromTXT += reading;
+        }
+        if (textFromTXT == "") {
+            return "Sorry, ASR could not understand you!";
+        }
+        return textFromTXT;
+
+    }
+}
 
